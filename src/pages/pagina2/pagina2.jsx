bdb6979bcaacf26d80aad8d0f94c2a5b6bde90d9
Karol <karol.danttas2303@gmail.com>
2025-05-20 15:48:49 -0300

    subindo o footer

--- src/pages/pagina2/pagina2.jsx
+++ src/pages/pagina2/pagina2.jsx
@@ -1,24 +1,21 @@
 import Card from "../../components/card/Card"
 import Filtro from "../../components/filtro/filtro"
+import Footer from "../../components/footer/footer"
 
 
 
 
 
 
-export default Pagina2(){
+export default function Pagina2(){
     return (
-       
-       <Card/>
-       <Filtro/>
-      
+        <>
 
+            <Card />
+            <Filtro />
+            <Footer />
 
-
-
-
-
-
+        </>
 
 
     );
